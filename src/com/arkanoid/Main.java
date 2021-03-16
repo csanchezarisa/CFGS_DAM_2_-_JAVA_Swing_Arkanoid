d3aa5ff489d2d11b00d9fcd992425501829d8b94
ulkiobal <ulkiobal@users.noreply.github.com>
2021-03-16 17:20:58 +0100

    Label en la parte superior con la score incremental

--- src/com/arkanoid/Main.java
+++ src/com/arkanoid/Main.java
@@ -7,6 +7,8 @@ import java.awt.*;
 public class Main {
 
     public static JFrame MAIN_FRAME;
+    public static JLabel SCORE_LABEL;
+    public static Game game;
 
     /** Método principal que inicia el juego */
     public static void main(String[] args) throws InterruptedException {
@@ -15,24 +17,29 @@ public class Main {
         MAIN_FRAME = new JFrame("Arkanoid");
         MAIN_FRAME.setSize(300, 400);
         MAIN_FRAME.setLayout(new BorderLayout());
-
-        JLabel lable = new JLabel("Hola");
+        SCORE_LABEL = new JLabel("Score: 0");
+        SCORE_LABEL.setFont(new Font("Verdana", Font.BOLD, 18));
 
         // Se inicializa la clase juego
-        Game game = new Game();
+        game = new Game();
         MAIN_FRAME.add(game, "Center");
-        MAIN_FRAME.add(lable, "North");
+        MAIN_FRAME.add(SCORE_LABEL, "North");
         MAIN_FRAME.setResizable(false);
         MAIN_FRAME.setVisible(true);
         MAIN_FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Bucle principal del juego
         while (true) {
+            Main.paintScore();
             game.move();
             game.repaint();
             Thread.sleep(10);
         }
 
+    }
+
+    private static void paintScore() {
+        SCORE_LABEL.setText("Score: " + game.getScore());
     }
 
 }
