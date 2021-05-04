20da367e70f79718dda6d7a2d8e25a3e4e3cd7ae
ulkiobal <ulkiobal@users.noreply.github.com>
2021-05-04 18:56:04 +0200

    Test para movimiento bola

--- test/java/com/arkanoid/entities/BallTest.java
+++ test/java/com/arkanoid/entities/BallTest.java
@@ -18,4 +18,50 @@ public class BallTest {
         ball = new Ball(game);
         Assertions.assertNotNull(ball);
     }
+
+    @Test
+    void testLeftTouch() {
+        JFrame frame = new JFrame("Arkanoid test");
+        frame.setSize(500, 500);
+        Game game = new Game(frame);
+        ball = new Ball(game);
+        ball.x = -10;
+        ball.move();
+        Assertions.assertTrue(ball.xa > 0);
+    }
+
+    @Test
+    void testRightTouch() {
+        JFrame frame = new JFrame("Arkanoid test");
+        frame.setSize(500, 500);
+        Game game = new Game(frame);
+        ball = new Ball(game);
+        ball.x = frame.getWidth() + 1;
+        ball.move();
+        Assertions.assertTrue(ball.xa < 0);
+    }
+
+    @Test
+    void testTopTouch() {
+        JFrame frame = new JFrame("Arkanoid test");
+        frame.setSize(500, 500);
+        Game game = new Game(frame);
+        ball = new Ball(game);
+        ball.y = 0;
+        ball.move();
+        Assertions.assertTrue(ball.ya > 0);
+    }
+
+    @Test
+    void testBottomTouch() {
+        JFrame frame = new JFrame("Arkanoid test");
+        frame.setSize(500, 500);
+        Game game = new Game(frame);
+        game.setSize(500, 500);
+        ball = new Ball(game);
+        ball.y = frame.getHeight() + 1;
+        int lives = game.getLives();
+        ball.move();
+        Assertions.assertFalse(game.getLives() == lives);
+    }
 }
